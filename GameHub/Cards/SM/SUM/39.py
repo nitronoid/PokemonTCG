cc4660c534270916a047f4769c9eb239cc337f97
mshooter <moira.shooter@hotmail.com>
2017-11-20 15:00:31 +0000

    added 2 cards

---
+++ GameHub/Cards/SM/SUM/39.py
@@ -0,0 +1,7 @@
+import poke
+
+def pound(h):
+    h.dealDamage(10)
+
+def waterGun(h)
+    h.dealDamage(20)
---
+++ GameHub/Cards/SM/SUM/40.py
@@ -0,0 +1,8 @@
+import poke
+
+def waveSplash(h)
+    h.dealDamage(30)
+
+def DisarmingVoice(h)
+    h.dealDamage(50)
+    h.applyCondition(CONFUSED, ACTIVE, ENEMY)

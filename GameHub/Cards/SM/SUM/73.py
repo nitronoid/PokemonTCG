196c17ede1f9568eec7836205f95996324c76789
ThomasRoper <s4913455@w11005.student.bournemouth.ac.uk>
2017-11-20 16:24:30 +0000

    added Passimain RH

---
+++ GameHub/Cards/SM/SUM/73.py
@@ -0,0 +1,8 @@
+import poke
+
+def bite(h):
+    h.dealDamage(30 #needs to do damage to a benched pokemon after selection)
+
+
+def flare(h): 
+    h.dealDamage(10#10 +30 for every pokemon on the bench)

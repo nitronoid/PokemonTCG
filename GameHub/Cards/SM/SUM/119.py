7d651e75d4308a9c464adc72fac08dcf95649691
nitronoid <jackdiver@hotmail.co.uk>
2017-11-27 13:13:56 +0000

    Fixed chooseCard again

--- GameHub/Cards/SM/SUM/119.py
+++ GameHub/Cards/SM/SUM/119.py
@@ -1,8 +1,9 @@
 import poke
 
 def greatBall(h):
-    # std::vector<int> Game::playerChoice(PLAYER player, PILE origin, CARD cardType, ACTION action, int amount = 1, int range = origin.size);
-    # player - Player who makes the choice (SELF/ENEMY)
+    # std::vector<int> Game::playerChoice(PLAYER thinker, PLAYER owner, PILE origin, CARD cardType, ACTION action, int amount = 1, int range = origin.size);
+    # thinker - Player who makes the choice (SELF/ENEMY)
+    # owner - The player who owns the cards to choose from
     # origin - Search location (DECK/HAND/DISCARD/BENCH/PRIZE)
     # cardType - The cards to select (POKEMON/SUPPORTER/ENERGY.....ALL)
     # action - what to do with the selection
@@ -10,13 +11,14 @@ def greatBall(h):
     # range - Number of cards the player picks from (>1), defaults to the whole thing
     # return - The list of chosen cards
 
-    cards = h.playerChoice(SELF, DECK, POKEMON, DRAW, 1, 7)
+    cards = h.playerChoice(SELF, SELF, DECK, POKEMON, DRAW, 1, 7)
 
-    # void Game::moveCards(std::vector<int> cardIndices, PILE origin, PILE destination, bool reveal);
+    # void Game::moveCards(std::vector<int> cardIndices, PLAYER owner, PILE origin, PILE destination, bool reveal);
     # cardIndices - Indices of card in Origin
+    # owner - The player who owns the cards to move
     # origin - card's original location (DECK/HAND/DISCARD/BENCH/PRIZE)
     # destination - where the cards goes (DECK/HAND/DISCARD/BENCH/PRIZE)
     # reveal - whether the enemy sees the card (True/False)
     
-    h.moveCards(cards, DECK, HAND, True)
+    h.moveCards(cards, SELF, DECK, HAND, True)
     h.shuffleDeck()
--- GameHub/Cards/SM/SUM/135.py
+++ GameHub/Cards/SM/SUM/135.py
@@ -1,8 +1,9 @@
 import poke
 
 def ultraBall(h):
-    # std::vector<int> Game::playerChoice(PLAYER player, PILE origin, CARD cardType, ACTION action, int amount = 1, int range = origin.size);
-    # player - Player who makes the choice (SELF/ENEMY)
+    # std::vector<int> Game::playerChoice(PLAYER thinker, PLAYER owner, PILE origin, CARD cardType, ACTION action, int amount = 1, int range = origin.size);
+    # thinker - Player who makes the choice (SELF/ENEMY)
+    # owner - The player who owns the cards to choose from
     # origin - Search location (DECK/HAND/DISCARD/BENCH/PRIZE)
     # cardType - The cards to select (POKEMON/SUPPORTER/ENERGY.....ALL)
     # action - what to do with the selection
@@ -12,16 +13,17 @@ def ultraBall(h):
 
     # void Game::moveCards(std::vector<int> cardIndices, PILE origin, PILE destination, bool reveal);
     # cardIndices - Indices of card in Origin
+    # owner - The player who owns the cards to move
     # origin - card's original location (DECK/HAND/DISCARD/BENCH/PRIZE)
     # destination - where the cards goes (DECK/HAND/DISCARD/BENCH/PRIZE)
     # reveal - whether the enemy sees the card (True/False)
 
-    discardChoice = h.playerChoice(SELF, HAND, ANY, DISCARD, 2)
+    discardChoice = h.playerChoice(SELF, SELF, HAND, ANY, DISCARD, 2)
 
     if len(discardChoice) == 2 :
-        h.moveCards(discardChoice, HAND, DISCARD, False)
-        pokeCard = h.playerChoice(SELF, DECK, POKEMON, DRAW)
-        h.moveCards(pokeCard, DECK, HAND, True)
+        h.moveCards(discardChoice, SELF, HAND, DISCARD, False)
+        pokeCard = h.playerChoice(SELF, SELF, DECK, POKEMON, DRAW, 1)
+        h.moveCards(pokeCard, SELF, DECK, HAND, True)
         h.shuffleDeck()
 
 

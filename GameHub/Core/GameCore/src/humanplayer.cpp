d494f02bee6794e790de499053a6ae51e2345c93
Renats Bikmajevs <rbikmajevs@gmail.com>
2017-12-09 01:30:07 +0000

    HumanPlayer Updated for testing purposes

--- GameHub/Core/GameCore/src/humanplayer.cpp
+++ GameHub/Core/GameCore/src/humanplayer.cpp
@@ -1,4 +1,6 @@
 #include "humanplayer.h"
+#include <time.h>
+#include <random>
 
 Player* HumanPlayer::clone() const
 {
@@ -18,5 +20,12 @@ std::vector<std::unique_ptr<Card>> HumanPlayer::chooseCard(
 
 std::pair<bool, unsigned> HumanPlayer::turn()
 {
-  return {false, 0};
+  srand((unsigned)time(NULL));
+  playCard((int)rand%(int)(viewHand().size()));
+  bool temp= ((int)rand()%2 == 0 ? true : false);
+  unsigned choice=0;
+  do{
+    choice=(unsigned)(rand()%counter);
+  }while(viewBench().at(choice) != nullptr);
+  return std::pair<bool, unsigned> {temp,choice};
 }

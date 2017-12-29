0e2105e997ec895a789532338eef4dc6a7339904
Mshooter <moira.shooter@hotmail.com>
2017-12-29 23:21:38 +0100

    delete the for loop

--- GameHub/Cards/SM/SUM/97.py
+++ GameHub/Cards/SM/SUM/97.py
@@ -2,7 +2,6 @@ import poke
 
 def peckBugs(h):
     dmg = 10
-    for i in h.viewBench(ENEMY):
-        if(i.type() == GRASS):
-            dmg += 30
-        h.dealDamage(dmg)
+    if(h.viewBoard(ENEMY)[0].type() == GRASS):
+        dmg += 30
+    h.dealDamage(dmg)

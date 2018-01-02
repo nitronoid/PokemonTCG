9fa547e6eeeb44bafe6a1e0ea30d6eb245ed9627
Mshooter <moira.shooter@hotmail.com>
2018-01-02 22:01:22 +0100

    changed function to viewBench

--- GameHub/Cards/SM/SUM/50.py
+++ GameHub/Cards/SM/SUM/50.py
@@ -6,6 +6,6 @@ def confuseRay(h):
 def aquaSpark(h):
     dmg = 60
     # if water energy is attached to pokemon
-    if(h.viewBoard(p.PLAYER.SELF)[0].energy() == p.TYPE.WATER):
+    if(p.TYPE.WATER in h.viewBench(p.PLAYERS.SELF)[0].energySet()):
         dmg += 60
     h.dealDamage(dmg)

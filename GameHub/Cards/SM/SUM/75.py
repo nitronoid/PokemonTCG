622836111f80d413ae79c7e05a878f9b8c0660d2
Lucien Hugueniot <lhug@localhost.localdomain>
2018-01-07 15:30:20 +0000

    ayo i added some new cards dawg (75)

---
+++ GameHub/Cards/SM/SUM/75.py
@@ -0,0 +1,8 @@
+import poke
+
+def wallofSand(h):
+    h.dealDamage(0 ) #takes 20 less damage from attacks
+
+
+def absorbVitality(h): 
+    h.dealDamage(50) #heal from this pokemon same amount of damage as done to opponent active pokemon

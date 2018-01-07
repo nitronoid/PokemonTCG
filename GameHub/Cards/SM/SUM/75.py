737f9a56d8aa9d5ed7ae8f923b7fb7eb55038183
Lucien Hugueniot <lhug@localhost.localdomain>
2018-01-07 16:59:36 +0000

    ayo dawg fixed card 75 (Palossand)

--- GameHub/Cards/SM/SUM/75.py
+++ GameHub/Cards/SM/SUM/75.py
@@ -1,8 +1,13 @@
-import poke
+import poke as p
+
+
 
 def wallofSand(h):
-    h.dealDamage(0 ) #takes 20 less damage from attacks
+    h.addBonusDefense(20, p.ORDER.AFTER)
 
-
-def absorbVitality(h): 
+def absorbVitality(h):
+    healAmount=h.viewBench(p.PLAYER.ENEMY)[0].remainingHP()
+    healAmount=clamp(0,50,healAmount)
     h.dealDamage(50) #heal from this pokemon same amount of damage as done to opponent active pokemon
+    h.healDamage(healAmount)
+    

633fe57ddc47a92781143d4ee4e688afe52231ce
lhugueniot <lucien98@gmail.com>
2018-01-08 14:02:44 +0000

    ayo dawg added passimian(73)

--- GameHub/Cards/SM/SUM/73.py
+++ GameHub/Cards/SM/SUM/73.py
@@ -1,8 +1,16 @@
 import poke
 
+def filter(slot):
+    return slot.active() is not None
+
 def bite(h):
-    h.dealDamage(30 #needs to do damage to a benched pokemon after selection)
+    chosenEnemy=playerSlotChoice(p.PLAYER.SELF, p.PLAYER.ENEMY, p.ACTION.ATTACK,1,filter)[0] #
+    h.dealDamage(30, chosenEnemy, False) #needs to do damage to a benched pokemon after selection
 
 
-def flare(h): 
-    h.dealDamage(10#10 +30 for every pokemon on the bench)
+def flare(h):
+    count=0
+    for slot in h.viewBench(p.PLAYER.SELF):
+        if slot.active().getName() == "Passimian":
+            count+=1
+    h.dealDamage(10+30*count)#10 +30 for every pokemon on the bench

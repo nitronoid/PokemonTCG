af5e92f3a1b072c9cf0bb5579d828062389e23d8
mshooter <moira.shooter@hotmail.com>
2018-01-09 14:41:57 +0000

    add file for behaviour tree main.cpp

--- GameHub/Core/behaviourTree/main.cpp
+++ GameHub/Core/behaviourTree/main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include "node.h"
+#include "action.h"
+#include "sequence.h"
 
-using namespace std;
+/// #include <chrono>
+/// #include <thread>
+/// std::this_thread::sleep_for(std::chrono::milliseconds(800));
+
+/// build test tree
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // root node is a Sequence because it is going through a oder
+    // attack is the last action you are doing
+    Sequence* actionOrder = new Sequence;
+    actionOrder->addChild(new Action);
+    while(!actionOrder->execute())
+        std::cout<<"STOPPED"<<'\n';
     return 0;
 }

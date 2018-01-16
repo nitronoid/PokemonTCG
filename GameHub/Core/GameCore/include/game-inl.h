38358e77ff829fc44bc9363f9f731136ea90651e
nitronoid <jackdiver@hotmail.co.uk>
2018-01-16 01:18:31 +0000

    simplified template cases slightly with a using declaration

--- GameHub/Core/GameCore/include/game-inl.h
+++ GameHub/Core/GameCore/include/game-inl.h
@@ -6,8 +6,9 @@
 // Use this namespace to prevent polluting the public namespace and to hide implementation details
 namespace _PTCG_DETAIL
 {
+using event = Game::Event;
 // Alias to simplify template declarations, simply checks two events for equality
-template<Game::Event k_eventA, Game::Event k_eventB>
+template<event k_eventA, event k_eventB>
 using MatchEvent = typename std::enable_if_t<k_eventA == k_eventB>;
 
 // observerCase can be thought of like a case in a switch statement. Every overload is a different case.
@@ -15,42 +16,42 @@ using MatchEvent = typename std::enable_if_t<k_eventA == k_eventB>;
 // First arg is the Event enum, second is a list of args to be forwarded to the guimodule call.
 
 // This case is compiled when the passed Event was START_TURN
-template<Game::Event k_event, typename... Args, MatchEvent<k_event, Game::Event::START_TURN>* = nullptr>
+template<event k_event, typename... Args, MatchEvent<k_event, event::START_TURN>* = nullptr>
 void observerCase(GameObserver*io_observer, Args&&...args)
 {
   io_observer->startTurn(std::forward<Args>(args)...);
 }
 
 // This case is compiled when the passed Event was EFFECT_USED
-template<Game::Event k_event, typename... Args, MatchEvent<k_event, Game::Event::EFFECT_USED>* = nullptr>
+template<event k_event, typename... Args, MatchEvent<k_event, event::EFFECT_USED>* = nullptr>
 void observerCase(GameObserver*io_observer, Args&&...args)
 {
   io_observer->effectUsed(std::forward<Args>(args)...);
 }
 
 // This case is compiled when the passed Event was ATTACK
-template<Game::Event k_event, typename... Args, MatchEvent<k_event, Game::Event::ATTACK>* = nullptr>
+template<event k_event, typename... Args, MatchEvent<k_event, event::ATTACK>* = nullptr>
 void observerCase(GameObserver*io_observer, Args&&...args)
 {
   io_observer->attackUsed(std::forward<Args>(args)...);
 }
 
 // This case is compiled when the passed Event was PLAY_CARD
-template<Game::Event k_event, typename... Args, MatchEvent<k_event, Game::Event::PLAY_CARD>* = nullptr>
+template<event k_event, typename... Args, MatchEvent<k_event, event::PLAY_CARD>* = nullptr>
 void observerCase(GameObserver*io_observer, Args&&...args)
 {
   io_observer->playCard(std::forward<Args>(args)...);
 }
 
 // This case is compiled when the passed Event was SWAP_SLOT
-template<Game::Event k_event, typename... Args, MatchEvent<k_event, Game::Event::SWAP_SLOT>* = nullptr>
+template<event k_event, typename... Args, MatchEvent<k_event, event::SWAP_SLOT>* = nullptr>
 void observerCase(GameObserver*io_observer, Args&&...args)
 {
   io_observer->swapSlot(std::forward<Args>(args)...);
 }
 
 // This case is compiled when the passed Event was MOVE_CARD
-template<Game::Event k_event, typename... Args, MatchEvent<k_event, Game::Event::MOVE_CARD>* = nullptr>
+template<event k_event, typename... Args, MatchEvent<k_event, event::MOVE_CARD>* = nullptr>
 void observerCase(GameObserver*io_observer, Args&&...args)
 {
   io_observer->moveCard(std::forward<Args>(args)...);
@@ -71,12 +72,11 @@ void observerCase(GameObserver*io_observer, Args&&...args)
 }
 
 // This case is compiled when the passed Event was INSPECT_CARD
-template<Game::Event k_event, typename... Args, MatchEvent<k_event, Game::Event::INSPECT_CARD>* = nullptr>
+template<event k_event, typename... Args, MatchEvent<k_event, event::INSPECT_CARD>* = nullptr>
 void observerCase(GameObserver*io_observer, Args&&...args)
 {
   io_observer->inspectCard(std::forward<Args>(args)...);
 }
-
 }
 
 

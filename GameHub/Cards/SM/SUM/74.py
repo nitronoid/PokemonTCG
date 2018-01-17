f25ebe9b27cf83df22ec490af05ab80adcff9463
nitronoid <jackdiver@hotmail.co.uk>
2018-01-17 23:02:13 +0000

    fixed card 74

--- GameHub/Cards/SM/SUM/74.py
+++ GameHub/Cards/SM/SUM/74.py
@@ -1,17 +1,18 @@
 import poke as p
 
+def match(card):
+    return card.cardType() == p.CARD.ENERGY and card.type() == p.TYPE.FIGHTING
+
 def shoreUp(h):
-    discard=h.viewDiscard(p.PLAYER.SELF)
-    indices = []
-    for i in range(discard):
-        if discard[i].cardType()== p.CARD.ENERGY and discard[i].type() == p.TYPE.FIGHTING:
-            indices.append(i)
-    h.pileToBench(p.PLAYER.SELF, p.PILE.DISCARD,indices[:1],[0]) #sort this bit out
+    choice = h.playerCardChoice(
+        p.PLAYER.SELF,
+        p.PLAYER.SELF,
+        p.PILE.DISCARD,
+        p.ACTION.PLAY,
+        match,
+        1)
+    if choice:
+        h.pileToBench(p.PLAYER.SELF, p.PILE.DISCARD, choice, [0]) #sort this bit out
 
 def sandTomb(h):
     h.dealDamage(30)
-    
-print "needs added enemy retreat function"
-
-
-
